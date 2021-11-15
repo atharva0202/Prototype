832ee6fa72b918b0d378651ceccb5745bef474ab
Namita18 <atharvavohra132@gmail.com>
2021-11-15 19:14:10 +0530

    app1

---
+++ server/client/build/precache-manifest.bbbeef68fd4a49122b59287c6b5754d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "873e208145950d4653e03b7a45ecbe74",
+    "url": "/index.html"
+  },
+  {
+    "revision": "700d410a9bd111579d42",
+    "url": "/static/css/main.c585ed2f.chunk.css"
+  },
+  {
+    "revision": "8c205bc5a75a1646c920",
+    "url": "/static/js/2.0f9dfa07.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.0f9dfa07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "700d410a9bd111579d42",
+    "url": "/static/js/main.ec98d402.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
